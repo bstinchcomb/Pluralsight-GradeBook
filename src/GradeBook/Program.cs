69b4c0c816c1b7ae3528897f5350db6c5476d812
Bradley Stinchcomb <bradley.stinchcomb@gmail.com>
2021-02-14 16:29:11 -0700

    Project updated

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+	        {	        
+	        	Console.WriteLine($"Hello {args[0]}!");
+	        }
+	        catch (Exception)
+	        {
+                Console.WriteLine("Oops, got exception " + Exception);
+	        }
+        }
+    }
+}
